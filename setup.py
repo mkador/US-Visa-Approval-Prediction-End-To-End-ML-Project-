efffa65050ff6ff30ffdd70ad087ba1969aa0545
Mk Adore <mkador169@gmil.com>
2025-08-29 22:14:39 +0600

    install requirements and template setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Md. Musa Kalimulla",
+    author_email="mkador169@gmail.com",
+    packages=find_packages()
+)
