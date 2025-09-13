152ece5a849ce9ea7812a5b5994b6b7f17924adf
Mk Adore <mkador169@gmil.com>
2025-09-13 19:10:19 +0600

    data ingestion components added

--- demo.py
+++ demo.py
@@ -1,11 +1,21 @@
-from us_visa.logger import logging
-from us_visa.exception import USvisaException
-# logging.info("This is my log mcg")
-import sys
-try:
-    r=3/0
-    print('r')
-except Exception as e:
-    #if i want save this error in log
-    # logging.info(e)
-    raise USvisaException(e,sys)
+# from us_visa.logger import logging
+# from us_visa.exception import USvisaException
+# # logging.info("This is my log mcg")
+# import sys
+# try:
+#     r=3/0
+#     print('r')
+# except Exception as e:
+#     #if i want save this error in log
+#     # logging.info(e)
+#     raise USvisaException(e,sys)
+
+# import os 
+# mongodburl = os.getenv("MONGODB_URL")
+# print(mongodburl)
+
+
+from us_visa.pipline.training_pipeline import TrainingPipeline
+
+pipeline = TrainingPipeline()
+pipeline.run_pipeline()
